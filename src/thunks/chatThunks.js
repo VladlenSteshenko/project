d4ff28c968776736bc525fedfa9721e1924ce8c4
VladlenSteshenko <vustesha2002@gmail.com>
2024-07-31 16:27:58 +0300

    socketlive

--- src/thunks/chatThunks.js
+++ src/thunks/chatThunks.js
@@ -7,7 +7,9 @@ import {
   setSelectedChatMessages,
   addMessage,
 } from '../reducers/chatSlice';
-import { io} from 'socket.io-client';
+import io from 'socket.io-client';
+
+const socket = io("ws://chat.ed.asmer.org.ua")
 
 export const connectSocket = createAsyncThunk(
   'auth/connectSocket',
@@ -15,17 +17,25 @@ export const connectSocket = createAsyncThunk(
     const state = getState();
     const token = state.auth.token;
     console.log(11111111111111,token)
+    if (!token) {
+      console.error("User not authenticated");
+      return;
+    }
+
     // Connect to socket and authenticate
-    const socket = io("ws://chat.ed.asmer.org.ua")
+    
     socket.emit('jwt', token);
 
     // Receive updates for chats of user
-    socket.on('chat', (chats) => {
-      const chatMap = {};
+    socket.on('chat', (chat) => {
+      console.log(3333333333333333333333,chat)
+      /*const chatMap = {};
       chats.forEach(chat => {
         chatMap[chat._id] = chat;
       });
-      dispatch(setChatList(chatMap));
+      dispatch(setChatList(chatMap));*/
+      dispatch(addChat(chat));
+
     });
 
     // Receive messages for selected chat
